654cdb87760bd9c7a4b41e5d156b81b8f15b7bf2
sino- <sinistersino@gmail.com>
2019-07-27 22:53:50 -0700

    refactored to use promises

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const request = require('request');
+const request = require('request-promise-native');
 const draw = require('iterm2-image');
 const https = require('https');
 const argv = require('yargs').argv;
@@ -10,34 +10,59 @@ const keyStr = 'client_id=' + unsplashKey;
 const itemList = argv._;
 const urlWithKey = url + '?' + keyStr + '&query=';
 
+// function getPicturesFromList(list) {
+//     const images = [];
+//     let counter = 0;
+//     list.forEach( (item) => {
+//         request({url: (urlWithKey + item), json: true}, (error, response) => {
+//             if (error) {
+//                 console.log(error);
+//             } else {
+//                 images.push(response.body.results[0].urls.thumb);
+//             }
+//             if (++counter === list.length) {
+//                 images.forEach((image) => {
+//                     if (process.env.TERM_PROGRAM === 'iTerm.app') {
+//                         https.get(image, function(res, err) {
+//                             if (res.statusCode === 200) {
+//                                 draw(res, function (err) {
+//                                     if (err) { throw err; }
+//                                 });
+//                             }
+//                         });
+//                     } else {
+//                         console.log(image);
+//                     }
+//                 });
+//             }
+//         });
+//     });
+//     return images;
+// }
+
 function getPicturesFromList(list) {
     const images = [];
     list.forEach( (item) => {
-        request({url: (urlWithKey + item), json: true}, (error, response) => {
-            if (error) {
-                console.log(error);
-            } else {
-                images.push(response.body.results[0].urls.thumb);
-            }
+        let something = request({url: (urlWithKey + item), json: true});
+        something.then(() => {
+        //    images.push(something.response.body.results[0].urls.thumb);
+           getImage(something.response.body.results[0].urls.thumb);
         });
     });
-    return images;
 }
 
-const images = getPicturesFromList(itemList);
-
-setTimeout( () => {
-    images.forEach((image) => {
-        if (process.env.TERM_PROGRAM === 'iTerm.app') {
-            https.get(image, function(res, err) {
-                if (res.statusCode === 200) {
-                    draw(res, function (err) {
+function getImage(url) {
+    if (process.env.TERM_PROGRAM === 'iTerm.app') {
+        https.get(url, function(res, err) {
+            if (res.statusCode === 200) {
+                draw(res, function (err) {
                     if (err) { throw err; }
-                    });
-                }
-            });
-        } else {
-            console.log(image);
-        }
-    });
-},1000);
+                });
+            }
+        });
+    } else {
+        console.log(image);
+    }
+}
+
+getPicturesFromList(itemList);
