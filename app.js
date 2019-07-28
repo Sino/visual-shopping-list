9475509f6a765985e8a639290a96b22869711147
sino- <sinistersino@gmail.com>
2019-07-27 22:56:35 -0700

    refactored to use promises

--- app.js
+++ app.js
@@ -10,42 +10,11 @@ const keyStr = 'client_id=' + unsplashKey;
 const itemList = argv._;
 const urlWithKey = url + '?' + keyStr + '&query=';
 
-// function getPicturesFromList(list) {
-//     const images = [];
-//     let counter = 0;
-//     list.forEach( (item) => {
-//         request({url: (urlWithKey + item), json: true}, (error, response) => {
-//             if (error) {
-//                 console.log(error);
-//             } else {
-//                 images.push(response.body.results[0].urls.thumb);
-//             }
-//             if (++counter === list.length) {
-//                 images.forEach((image) => {
-//                     if (process.env.TERM_PROGRAM === 'iTerm.app') {
-//                         https.get(image, function(res, err) {
-//                             if (res.statusCode === 200) {
-//                                 draw(res, function (err) {
-//                                     if (err) { throw err; }
-//                                 });
-//                             }
-//                         });
-//                     } else {
-//                         console.log(image);
-//                     }
-//                 });
-//             }
-//         });
-//     });
-//     return images;
-// }
-
 function getPicturesFromList(list) {
     const images = [];
     list.forEach( (item) => {
         let something = request({url: (urlWithKey + item), json: true});
         something.then(() => {
-        //    images.push(something.response.body.results[0].urls.thumb);
            getImage(something.response.body.results[0].urls.thumb);
         });
     });
